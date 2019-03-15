aaa9db327ae67a0664c84a51a4060f62a99f7e3d
Neil Mispelaar <neil.mispelaar@canada.ca>
2019-03-15 11:32:06 -0400

    Setup chart js with basic sample data

--- static/js/plugins.js
+++ static/js/plugins.js
@@ -22,3 +22,140 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+new Chart(document.getElementById("strategic-design-skills"), {
+  type: 'radar',
+  data: {	
+    labels: ["Facilitation", "Design thinking", "Presenting", "Communication", "UX strategy and planning", "Negotiation"],
+    datasets: [
+      { 
+        label: "Current State",
+        fill: true,
+        backgroundColor: "rgba(179,181,198,0.2)",
+        borderColor: "rgba(179,181,198,1)",
+        pointBorderColor: "#fff",
+        pointBackgroundColor: "rgba(179,181,198,1)",
+        data: [0,2.0,3.0,2.0,2.0,2.0]
+      }, {
+        label: "Desired State",
+        fill: true,
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        pointBorderColor: "#fff",
+        pointBackgroundColor: "rgba(255,99,132,1)",
+        pointBorderColor: "#fff",
+        data: [3,2,2,3,3,2]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Other Skills'
+    }
+  }
+});
+
+
+new Chart(document.getElementById("design-skills"), {
+  type: 'radar',
+  data: {	
+    labels: ["Facilitation", "Design thinking", "Presenting", "Communication", "UX strategy and planning", "Negotiation"],
+    datasets: [
+      { 
+        label: "Current State",
+        fill: true,
+        backgroundColor: "rgba(179,181,198,0.2)",
+        borderColor: "rgba(179,181,198,1)",
+        pointBorderColor: "#fff",
+        pointBackgroundColor: "rgba(179,181,198,1)",
+        data: [0,2.0,3.0,2.0,2.0,2.0]
+      }, {
+        label: "Desired State",
+        fill: true,
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        pointBorderColor: "#fff",
+        pointBackgroundColor: "rgba(255,99,132,1)",
+        pointBorderColor: "#fff",
+        data: [3,2,2,3,3,2]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Other Skills'
+    }
+  }
+});
+
+
+
+new Chart(document.getElementById("development-skills"), {
+  type: 'radar',
+  data: {	
+    labels: ["Facilitation", "Design thinking", "Presenting", "Communication", "UX strategy and planning", "Negotiation"],
+    datasets: [
+      { 
+        label: "Current State",
+        fill: true,
+        backgroundColor: "rgba(179,181,198,0.2)",
+        borderColor: "rgba(179,181,198,1)",
+        pointBorderColor: "#fff",
+        pointBackgroundColor: "rgba(179,181,198,1)",
+        data: [0,2.0,3.0,2.0,2.0,2.0]
+      }, {
+        label: "Desired State",
+        fill: true,
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        pointBorderColor: "#fff",
+        pointBackgroundColor: "rgba(255,99,132,1)",
+        pointBorderColor: "#fff",
+        data: [3,2,2,3,3,2]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Other Skills'
+    }
+  }
+});
+
+
+
+new Chart(document.getElementById("other-skills"), {
+  type: 'radar',
+  data: {	
+    labels: ["Facilitation", "Design thinking", "Presenting", "Communication", "UX strategy and planning", "Negotiation"],
+    datasets: [
+      { 
+        label: "Current State",
+        fill: true,
+        backgroundColor: "rgba(179,181,198,0.2)",
+        borderColor: "rgba(179,181,198,1)",
+        pointBorderColor: "#fff",
+        pointBackgroundColor: "rgba(179,181,198,1)",
+        data: [0,2.0,3.0,2.0,2.0,2.0]
+      }, {
+        label: "Desired State",
+        fill: true,
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        pointBorderColor: "#fff",
+        pointBackgroundColor: "rgba(255,99,132,1)",
+        pointBorderColor: "#fff",
+        data: [3,2,2,3,3,2]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Other Skills'
+    }
+  }
+});
